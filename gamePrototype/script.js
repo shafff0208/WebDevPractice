042cd6104bce1f16416f8fa2707a93cecf550e98
Shafiezan Shari <shafiezan88@gmail.com>
2024-11-05 17:27:01 +0800

    fix

--- gamePrototype/script.js
+++ gamePrototype/script.js
@@ -1,13 +1,15 @@
 //Whoever you are, whatever you do, you do not belong here, 
-//this is the number manufactoring factory, stay away and whatever you do, 
-//do not change anything, I REPEAT DO NOT CHANGE anything
+//this is the number manufactoring factory, stay away and whatever
+//you do, do not change anything, I REPEAT DO NOT CHANGE anything
 
 //Time Element
 let interval;
 let time = 1000;
+let seconds = 0;
 
 //Main Num
 let mainNum = 0;
+let mainNumPS = 0;
 
 //Game States
 let state = 0;
@@ -16,7 +18,7 @@ let playState = 2;
 
 //Auto Increment
 let autoIncrementNum = 1;
-let currentAutoIncrement;
+let currentAutoIncrement = 1;
 
 //Increments
 let increment = 1;
@@ -27,20 +29,20 @@ let incrementCost = 10;
 let upgradeCost = 10;
 let pauseCost = 1000;
 
-
-
-state = playState;
-
 //Game Loop
 function incrementNum(){
+    //Start Game Loop
     if(mainNum == 0){
         interval = setInterval(() => {
             mainNum = mainNum + autoIncrementNum;
             document.getElementById("mainNum").innerText = mainNum;
+            document.getElementById("mainNumPS").innerText = mainNumPS; 
+            seconds++
         }, time);
     }
-        mainNum += increment;
-        document.getElementById("mainNum").innerText = mainNum;
+    mainNum += increment;
+    mainNumPS = 
+    document.getElementById("mainNum").innerText = mainNum;
 }
 
 //Upgrades
@@ -58,6 +60,7 @@ function upgradeAuto(){
     if(mainNum >= upgradeCost){
         autoIncrementNum+=1;
         currentAutoIncrement = autoIncrementNum;
+        mainNumPS = currentAutoIncrement;
         mainNum-= upgradeCost;
         upgradeCost = upgradeCost * 2;
         document.getElementById("upgradeCostText").innerText = upgradeCost;
@@ -84,6 +87,7 @@ function pause(){
 
 function checkState(){
     if(mainNum >= pauseCost){
+        mainNum -= pauseCost;
         state === pauseState ? play() : pause();
     }
 }
